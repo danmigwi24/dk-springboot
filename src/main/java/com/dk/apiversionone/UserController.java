baa0b0ea95184255e74c07ac2efaead39d759f12
MacBookPro <kimani.daniel@eclectics.io>
2024-01-08 19:00:04 +0300

    first commit

--- src/main/java/com/dk/apiversionone/UserController.java
+++ src/main/java/com/dk/apiversionone/UserController.java
@@ -5,29 +5,41 @@ import org.springframework.web.bind.annotation.*;
 
 //https://console.aiven.io/account/a4863a7baf4b/project/dk-springboot/services
 @RestController
+@RequestMapping(path = "/demo")
 public class UserController {
 
     @Autowired
     private  UserRepository userRepository;
-
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
     }
 
-
+    /**
+     *
+     * @param name
+     * @param email
+     * @return
+     *
+     * curl http://localhost:8080/demo/add -d name="kunal" -d email="dan@gmail.com"
+     */
     @PostMapping(path = "/add")
     public @ResponseBody User addNewUser(@RequestParam String name, @RequestParam String email){
-
+        //
         User user = new User();
         user.setName(name);
         user.setEmail(email);
+        //
         userRepository.save(user);
-
+        //
         return user;
     }
 
+    /**
+     *
+     * curl http://localhost:8080/demo/all
+     */
     @GetMapping(path = "/all")
     public  @ResponseBody Iterable<User> getAllUser(){
         return userRepository.findAll();
